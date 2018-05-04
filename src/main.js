ace37ebeed6de0e813baf5138c5952e43bf2b631
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-05-04 13:08:43 -0700

    Added terrain generation + smoothing

--- src/main.js
+++ src/main.js
@@ -1,34 +1,44 @@
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
-const { width: w, height: h } = canvas;
-/*
-for (let below = 1; below < 6; below++) {
-	for (let square = 1; square < 27; square++) {
-		//////////Draw red square with a black outline
-		ctx.strokeStyle = "black";
-		ctx.fillStyle = "red";
-		ctx.fillRect((50 * (square - 1)), (600 + (50 * below)), 50, 50);
-		ctx.strokeRect((50 * (square - 1)), (600 + (50 * below)), 50, 50);
-	}
-}
-*/
+//const { width: w, height: h } = canvas;
+//Width of squares
+var w = 1;
+//Height of squares
+var h = 1;
+//Create an array for the height of the columns
+var height = new Array();
+height[0] = 100;
+
+var index = 0;
+var xedni = 0;
+var value;
+
+console.log(height[0]);
+
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
-for (let column = 1; column < 27; column++) {
-	//////////Generate the height of each column	
-	random = getRandomInt(6, 10);
-	console.log(random);
-	for (let row = 1; row < random; row++) {
-		//////////Draw the red square with a black outline
-		ctx.strokeStyle = "black";
+for (let column = 0; column < Math.ceil(canvas.width / w); column++) {
+	//Generate the height of each column
+	random = getRandomInt((height[index] - 1), (height[index] + 1));
+	//Add height to the array
+	height.push(random);
+	//Set a variable equal to the length of the array
+	console.log(height[index]);
+	//Draw the columns with the height of their row number's index in height
+	for (let row = 0; row <= height[index]; row++) {
+		//Draw the squares
+		ctx.stokeStyle = "black";
 		ctx.fillStyle = "red";
-		ctx.fillRect((50 * (column - 1)), (900 - (50 * row)), 50, 50);
-		ctx.strokeRect((50 * (column - 1)), (900 - (50 * row)), 50, 50);
+		ctx.fillRect((w * index), (900 - (h * row)), w, h);
+		ctx.strokeRect((w * index), (900 - (h * row)), w, h);
 	}
+	
+	console.log("drawn" + index + "rows");
+	index++;
 }
+
+console.log(height);
